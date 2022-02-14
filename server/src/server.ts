a5671ca3509f8ecd0c1a8cf335e7c1af39026adf
Maxime Caricand <maxime.caricand@gmail.com>
2022-02-14 16:32:45 +0100

    fix node version issue + server comments

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,11 +8,10 @@ import SerialPort = require("serialport");
 const Readline = require('@serialport/parser-readline');
 
 const wsPort = 3100;
-const arduinoPort = 3200;
 const dbPort = 27017;
 const arduinoCOMPortUbuntu = '/dev/ttyACM0';
 const arduinoCOMPortMacos = '/dev/cu.usbmodem1101';
-const selectedArdCOMPort = arduinoCOMPortUbuntu;
+const selectedArdCOMPort = arduinoCOMPortMacos;
 var parser;
 
 (async () => {
@@ -24,20 +23,60 @@ var parser;
     arduinoSerialPort.on('open', function () {
         console.log(`[Serial Port] ${selectedArdCOMPort} is opened.`);
     });
-
     parser = arduinoSerialPort.pipe(new Readline({ delimiter: '\r\n' }));
     parser.on('data', (message: string) => { handleNewMessage(message, Date.now()); });
 
+    // Websocket setup
     const server = http.createServer();
     // @ts-ignore
     server.listen(wsPort, () => console.log('[WS] Listening on ' + server.address().address + ':' + server.address().port));
     const wss = new WebSocketServer({ server });
     wss.on('connection', () => console.log('new client'));
 
+    // broadcast a json to all connected clients
     function broadcastJSON(message: IScoreData | ILedMessageData) {
         wss.clients.forEach(client => {
             client.send(JSON.stringify(message));
         });
+    }
+
+    // handle new message from arduino
+    function handleNewMessage(message: string, date: number) {
+        const messageData = message.split(';');
+        switch (messageData[0]) {
+            case MessageHeader.LED:
+                return handleNewLed(+messageData[1]);
+            case MessageHeader.SCORE:
+                return handleNewScore(+messageData[1], date);
+        }
+    }
+
+    // update frontend with the last led
+    function handleNewLed(ledIndex: number) {
+        broadcastJSON({ type: MessageHeader.LED, curLed: ledIndex });
+    }
+
+    async function handleNewScore(score: number, date: number) {
+
+        // fetch average score and distribution from all db items
+        const queryResult = (await GameResultService.getScoreAvgAndDistribution());
+        const avgScore = queryResult.avg ?? score;
+        const category = getDistributionKey(score, avgScore);
+        const distribution = {
+            green: queryResult.green,
+            yellow: queryResult.yellow,
+            red: queryResult.red
+        }
+        distribution[category]++; // increase distribution with the new score
+
+        // Put new score item in db
+        await GameResultService.createGameResult(new GameResult({ score, category, datetime: date }));
+
+        // send led color back to arduino
+        arduinoSerialPort.write(category.charAt(0), 'ascii');
+
+        // update frontend
+        broadcastJSON({ type: MessageHeader.SCORE, score, avgScore, distribution });
     }
 
     function getDistributionKey(newValue: number, avg: number) {
@@ -50,47 +89,7 @@ var parser;
         }
     }
 
-    function handleNewMessage(message: string, date: number) {
-        const messageData = message.split(';');
-        switch (messageData[0]) {
-            case MessageHeader.LED:
-                return handleNewLed(+messageData[1]);
-            case MessageHeader.SCORE:
-                return handleNewScore(+messageData[1], date);
-        }
-    }
-
-    function handleNewLed(ledIndex: number) {
-        broadcastJSON({ type: MessageHeader.LED, curLed: ledIndex });
-    }
-
-    async function handleNewScore(score: number, date: number) {
-
-        const queryResult = (await GameResultService.getScoreAvgAndDistribution());
-        const averageScore = queryResult.avg ?? score;
-        const category = getDistributionKey(score, averageScore);
-        const distribution = {
-            green: queryResult.green,
-            yellow: queryResult.yellow,
-            red: queryResult.red
-        }
-        distribution[category]++; // increase distribution with the new score
-        await GameResultService.createGameResult(new GameResult({ score, category, datetime: date }));
-
-        sendCategoryToArduino(category);
-
-        broadcastJSON({
-            type: MessageHeader.SCORE,
-            score,
-            avgScore: averageScore,
-            distribution
-        });
-    }
-
-    function sendCategoryToArduino(category: DistributionKey) {
-        arduinoSerialPort.write(category.charAt(0), 'ascii');
-    }
-
+    // allow to test without the arduino board
     async function debug() {
         const ledAction = async function () {
             return new Promise((resolve) => {
@@ -113,7 +112,6 @@ var parser;
             await scoreAction();
         }
     };
-
     //debug();
 })();
 
